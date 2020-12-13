59b07f03e7e9be6c94143c00faec254e1c97f623
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2020-12-14 00:19:22 +0400

    creating new GUI
    adding Login page (not functional except Login button)
    adding Messages page (not functional except Back button)

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+from gui.myApp import Ui_MainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (QApplication, QMainWindow)
 import sys
-from PyQt5.QtWidgets import QApplication
-from gui.window import Window
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.ui = Ui_MainWindow()
+
+        self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.backButton.clicked.connect(self.back_to_login)
+        self.ui.loginButton.clicked.connect(self.login_to_messages)
+
+    def back_to_login(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def login_to_messages(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = Window()
-    main_window.show()
+    win = Window()
 
+    win.show()
     sys.exit(app.exec_())
